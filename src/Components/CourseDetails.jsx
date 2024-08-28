1b8cb666d9be8ed5cf24be9eb552e6627e5837a1
Aashwath Yadav <yadavar_7@rknec.edu>
2024-08-28 23:15:25 +0530

    Add initial files

---
+++ src/Components/CourseDetails.jsx
@@ -0,0 +1,14 @@
+import './CourseDetails.css';
+
+function CourseDetails({ course, onBack }) {
+  return (
+    <div className="course-details">
+      <button onClick={onBack}>Back to Courses</button>
+      <img src={course.image} alt={course.title} />
+      <h2>{course.title}</h2>
+      <p>{course.longDescription}</p>
+    </div>
+  );
+}
+
+export default CourseDetails;
