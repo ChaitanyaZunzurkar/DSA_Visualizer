d5dba4d34828ba677d9d33fbc9e8d58bd8d152fd
Aryan <aryanparas2004@gmail.com>
2024-10-02 10:41:06 +0530

    added a extra div named contentWrapper to help in css

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -4,11 +4,16 @@ import styles from '../Stylesheets/CourseCard.module.css';
 function CourseCard({ course, onClick }) {
   return (
     <div className={styles.courseCard} onClick={() => onClick(course)}>
-      <img src={course.image} alt={course.title} className={styles.image} />
-      <h3 className={styles.title}>{course.title}</h3>
-      <p className={styles.description}>{course.description}</p>
+      {/* Add a wrapper div */}
+      <div className={styles.contentWrapper}>
+        <img src={course.image} alt={course.title} className={styles.image} />
+        <h3 className={styles.title}>{course.title}</h3>
+        <p className={styles.description}>{course.description}</p>
+      </div>
+      {/* div ends here */}
     </div>
   );
 }
+
 
 export default CourseCard;
