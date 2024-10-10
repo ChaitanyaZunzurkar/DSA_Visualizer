2777ec69f5110bafb0f6dd8a69bd95c9a8c26368
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2024-10-10 16:54:41 +0530

    Adding Linkedlist routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
-import './App.css'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import './App.css';
+import { BrowserRouter, Routes, Route , Navigate} from 'react-router-dom';
 import Navbar from '../src/Components/Navbar.jsx';
 import Footer from '../src/Components/Footer.jsx';
 import HomePage from './Pages/HomePage.jsx';
 import About from './Pages/About.jsx';
-import Courses from './Pages/Courses.jsx'
+import Courses from './Pages/Courses.jsx';
 import COURSES from './Components/COURSES.jsx';
 import SideNav from './Components/SideNavbar';
 
+// Linked list page components
+import LinkedlistPage from './Algorithms/Linkedlist/LinkedlistPage.jsx';
+import Aim from './Algorithms/Linkedlist/Aim.jsx';
+import Articles from './Algorithms/Linkedlist/Articles.jsx';
+import Test from './Algorithms/Linkedlist/Test.jsx';
+import Tutorials from './Algorithms/Linkedlist/Tutorials.jsx';
+import Visualization from './Algorithms/Linkedlist/Visualization.jsx';
+
 function App() {
   return (
-    <>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route element={<HomePage/>} path="/" />
-          <Route element={<About/>} path="/about" />
+          <Route element={<HomePage />} path="/" />
+          <Route element={<About />} path="/about" />
           <Route path="/courses" element={<Courses />} />
           <Route path="/courses/:courseId" element={<COURSES />} />
-          <Route path='/courses/:courseId/:courseTitle' element={<SideNav/>} />
+          <Route path="/courses/:courseId/:courseTitle" element={<SideNav />} />
+          
+          <Route path="/courses/:courseId/Linked List" element={<LinkedlistPage />}>
+            <Route index element={<Aim />} />
+            <Route path="visualization" element={<Visualization />} />
+            <Route path="test" element={<Test />} />
+            <Route path="articles" element={<Articles />} />
+            <Route path="tutorials" element={<Tutorials />} />
+            <Route path="*" element={<Navigate to={`./`} />} />
+          </Route>
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
