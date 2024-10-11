0846ff7c51b287d72fedc4173c12b364e292055e
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2024-10-11 07:34:08 +0530

    removing Dijkstra algorithm

--- src/Algorithms/Dijkstra/Tutorials.jsx
+++
@@ -1,9 +0,0 @@
-const Tutorials = () => {
-    return (
-        <div>
-            <h1>This is Tutorials page</h1>
-        </div>
-    );
-}
-
-export default Tutorials;
--- src/Algorithms/Dijkstra/Visualization.jsx
+++
@@ -1,9 +0,0 @@
-const Visualization = () => {
-    return (
-        <div>
-            <h1>This is Visualization page</h1>
-        </div>
-    );
-}
-
-export default Visualization;
