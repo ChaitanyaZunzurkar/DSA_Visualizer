441cbbd73018173012c86f289eb4be2672a9f3cf
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2024-10-24 20:25:22 +0530

    Adding sideNav

--- src/Algorithms/Linkedlist/Linkedlist.jsx
+++ src/Algorithms/Linkedlist/Linkedlist.jsx
@@ -1,35 +1,3 @@
-// import { Link } from 'react-router-dom';
-// import styles from '../../Stylesheets/Linkedlist.module.css';
-
-// const LinkedList = () => {
-//     return (
-//         <div className={styles.container}>
-//             <nav className={styles.nav}>
-//                 <ul className={styles.ul}>
-//                     <li className={styles.li}>
-//                         <Link to="" className={styles.a}>Aim</Link>
-//                     </li>
-//                     <li className={styles.li}>
-//                         i want to add submenu in this sections named visualization
-//                     </li>
-//                     <li className={styles.li}>
-//                         <Link to="test" className={styles.a}>Test</Link>
-//                     </li>
-//                     <li className={styles.li}>
-//                         <Link to="articles" className={styles.a}>Articles</Link>
-//                     </li>
-//                     <li className={styles.li}>
-//                         <Link to="tutorials" className={styles.a}>Tutorials</Link>
-//                     </li>
-//                 </ul>
-//             </nav>
-//         </div>
-//     );
-// }
-
-// export default LinkedList;
-
-
 import { Link } from 'react-router-dom';
 import { useState } from 'react'; 
 import styles from '../../Stylesheets/Linkedlist.module.css';
@@ -54,7 +22,7 @@ const LinkedList = () => {
                     <li className={styles.li}>
                         <Link to="type" className={styles.a}>Types of Linked Lists</Link>
                     </li>
-                    <li className={styles.li}>
+                    <li className={styles.li} id="SpecialLi" style={{ paddingTop: '0px', paddingBottom: '0px' }}>
                         <div className={styles.box}>
                             <button onClick={toggleSubMenu} className={styles.btn}>
                                 Visualization
