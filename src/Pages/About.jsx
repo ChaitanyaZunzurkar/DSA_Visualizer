469a444cd7d56fa565e234b7be971dd2b96731d9
Aryan <aryanparas2004@gmail.com>
2024-10-24 18:30:00 +0530

    Changed names to reduce clashes in About.jsx and Added 3 course card , organizing algorithms presented

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -68,11 +68,11 @@ export default function TeamPage() {
     <StyledWrapper>
       <div className="teamContainer">
         {teamMembers.map(member => (
-          <div key={member.id} className="card">
+          <div key={member.id} className="teamCard">
             <div className="img" style={{ backgroundImage: `url(${member.image})` }} />
             <span><h3>{member.name}</h3></span>
-            <p className="info">{member.about}</p>
-            <div className="share">
+            <p className="teamInfo">{member.about}</p>
+            <div className="teamShare">
               <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
               <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
@@ -170,7 +170,7 @@ const StyledWrapper = styled.div`
     flex-wrap: wrap;
     padding: 30px 10px; // Increased padding for spacing
   }
-  .card {
+  .teamCard {
     width: 20em;
     margin:20px;
     
@@ -199,7 +199,7 @@ const StyledWrapper = styled.div`
     box-shadow: 0 4px 20px rgba(0, 0, 0, 0); /* Shadow effect */
   }
   }
-  .card span{
+  .teamCard span{
     font-weight: bold;
     color: white;
     text-align: center;
@@ -213,7 +213,7 @@ const StyledWrapper = styled.div`
     border-radius: 15px;
     margin: auto;
   }
-  .info {
+  .teamInfo {
     font-weight: 400;
     color: white;
     display: block;
@@ -222,7 +222,7 @@ const StyledWrapper = styled.div`
     margin: 1em;
     border: white;
   }
-  .share {
+  .teamShare {
      margin-top: 1em;
     display: flex;
     justify-content: center;
@@ -240,12 +240,12 @@ const StyledWrapper = styled.div`
     font-size: 1rem; // Font size for position
     color: #888; // Lighter color for position
   }
-  .card a {
+  .teamShare a {
     color: white;
     transition: 0.2s ease-in-out;
   }
 
-  .card a:hover {
+  .teamShare a:hover {
   color: #28a745;
   }
 `;
